3b8a7d425f12d8a990b0514fc89a60695263f09f
demarcod1 <miaouaf5040@gmail.com>
2020-12-26 11:44:31 -0500

    version update

--- librarianz_scripts.py
+++ librarianz_scripts.py
@@ -14,7 +14,7 @@ from scripts.remake_shortcuts import remake_shortcuts
 from scripts.upload_files import upload_files
 from scripts.validate_folder_files import validate_folder_files
 
-VERSION = '0.1.4'
+VERSION = '0.1.5'
 
 SCRIPT_DICT = {
     "download_parts": download_parts,
